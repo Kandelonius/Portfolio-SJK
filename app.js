847488c3be7362260a590421fff506317ea7b24a
Shane Kaestner <shane.kaestner@yahoo.com>
2020-04-11 22:24:23 -0700

    got color changing background working

---
+++ app.js
@@ -0,0 +1,10 @@
+function colorChange(event){
+    let documentRed = 255*event.offsetX/document.body.clientWidth;
+
+    let documentGreen = 255*event.clientY/screen.height;
+
+    let documentBlue = 255 - 255*event.offsetX/document.body.clientWidth;
+    document.body.style.backgroundColor = 'rgb(' + documentRed + ',' + documentGreen + ',' + documentBlue + ')';
+  }
+  
+  document.addEventListener('mousemove', colorChange);
