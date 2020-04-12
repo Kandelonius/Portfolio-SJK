7c7aa9dc9d70057613e63058b596dd88eca912c3
Shane Kaestner <Shane.Kaestner@yahoo.com>
2020-04-12 07:57:53 -0700

    got the color changing background working for the body ane contianer classes.

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-const container = document.querySelector(".container");
 function colorChange(event){
-    let documentRed = 255*event.offsetX/document.body.clientWidth;
-    let documentGreen = 255*event.clientY/screen.height;
-    let documentBlue = 255 - 255*event.offsetX/document.body.clientWidth;
-    container.style.backgroundColor = 'rgb(' + documentRed + ',' + documentGreen + ',' + documentBlue + ')';
-    document.body.style.backgroundColor = 'rgb(' + documentRed + ',' + documentGreen + ',' + documentBlue + ')';
-  }
+  const container = document.getElementsByClassName("container");
+  let documentRed = 255*event.offsetX/document.body.clientWidth;
+  let documentGreen = 255*event.clientY/screen.height;
+  let documentBlue = 255 - 255*event.offsetX/document.body.clientWidth;
+  for(i=0;i<container.length;i++){
+  container[i].style.backgroundColor = 'rgb(' + documentRed + ',' + documentGreen + ',' + documentBlue + ')';
+}
+  document.body.style.backgroundColor = 'rgb(' + documentRed + ',' + documentGreen + ',' + documentBlue + ')';
+}
   document.addEventListener('mousemove', colorChange);
-  // container.addEventListener('mousemove', colorChange(container));
+  
