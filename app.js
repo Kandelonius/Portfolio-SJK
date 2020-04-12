d335f43ee71a8ca06d562342b73240637de7a4e5
Shane Kaestner <Shane.Kaestner@yahoo.com>
2020-04-12 02:15:15 -0700

    changed the color scheme dramatically.

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
+const container = document.querySelector(".container");
 function colorChange(event){
     let documentRed = 255*event.offsetX/document.body.clientWidth;
-
     let documentGreen = 255*event.clientY/screen.height;
-
     let documentBlue = 255 - 255*event.offsetX/document.body.clientWidth;
+    container.style.backgroundColor = 'rgb(' + documentRed + ',' + documentGreen + ',' + documentBlue + ')';
     document.body.style.backgroundColor = 'rgb(' + documentRed + ',' + documentGreen + ',' + documentBlue + ')';
   }
   document.addEventListener('mousemove', colorChange);
-  
+  // container.addEventListener('mousemove', colorChange(container));
