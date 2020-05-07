042c4a7569c8a91ceaf67465c75d3913d024f238
Shane Kaestner <shane.kaestner@yahoo.com>
2020-05-07 14:32:57 -0700

    added future plans to readme

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 
 My portfolio website. The background color adjusts with user mouse movements. I plan to add an event listener to freeze the color at a specific color in a future update.
 
+Want to implement dark-mode/ light-mode/ mix-mode.
--- app.js
+++ app.js
@@ -3,6 +3,8 @@ function colorChange(event){
   let documentRed = 255*event.offsetX/document.body.clientWidth;
   let documentGreen = 255*event.clientY/screen.height;
   let documentBlue = 255 - 255*event.offsetX/document.body.clientWidth;
+  let setColor = 'rgb(' + documentRed + ',' + documentGreen + ',' + documentBlue + ')'
+  console.log(event.offsetX);
   for(i=0;i<container.length;i++){
   container[i].style.backgroundColor = 'rgb(' + documentRed + ',' + documentGreen + ',' + documentBlue + ')';
 }
