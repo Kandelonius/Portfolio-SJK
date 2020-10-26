e3fbe64be653385a20fb276a6d2cae8f1c12f65d
Shane Kaestner <shane.kaestner@yahoo.com>
2020-10-25 19:03:02 -0700

    finally got the color change effect to work without clipping to different layers.

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ function colorChange(event){
   let documentRed = 255*event.offsetX/document.body.clientWidth;
   let documentGreen = 255*event.clientY/screen.height;
   let documentBlue = 255 - 255*event.offsetX/document.body.clientWidth;
-  let setColor = 'rgb(' + documentRed + ',' + documentGreen + ',' + documentBlue + ')'
+  let setColor = 'rgb(' + documentRed + ',' + documentGreen + ',' + documentBlue + ')';
   console.log(event.offsetX);
   for(i=0;i<container.length;i++){
   container[i].style.backgroundColor = 'rgb(' + documentRed + ',' + documentGreen + ',' + documentBlue + ')';
 }
   document.body.style.backgroundColor = 'rgb(' + documentRed + ',' + documentGreen + ',' + documentBlue + ')';
 }
-document.getElementById('full-screen').addEventListener('mousemove', colorChange);
+document.querySelector('.full-screen').addEventListener('mousemove', colorChange);
