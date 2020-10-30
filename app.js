69055730bf614afa4afe83c6048d180e8a31e5e6
Shane Kaestner <Shane.Kaestner@yahoo.com>
2020-10-29 21:48:36 -0700

    working on an event listener to stop the color change effect when the escape key is pressed.

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
+let colorListener = true;
+
 function colorChange(event){
   const container = document.getElementsByClassName("container");
   let documentRed = 255*event.offsetX/document.body.clientWidth;
   let documentGreen = 255*event.clientY/screen.height;
   let documentBlue = 255 - 255*event.offsetX/document.body.clientWidth;
   let setColor = 'rgb(' + documentRed + ',' + documentGreen + ',' + documentBlue + ')';
-  // console.log(event.offsetX);
   for(i=0;i<container.length;i++){
   container[i].style.backgroundColor = 'rgb(' + documentRed + ',' + documentGreen + ',' + documentBlue + ')';
 }
   document.body.style.backgroundColor = 'rgb(' + documentRed + ',' + documentGreen + ',' + documentBlue + ')';
 }
+
+function colorToggle(event){
+  if (colorListener){
+      document.querySelector('.full-screen').removeEventListener('mousemove', 
+      colorChange);
+      colorListener = false;
+  }else{
+      document.querySelector('.full-screen').addEventListener('mousemove', 
+      colorChange);
+      colorListener = true;
+  }
+}
 document.querySelector('.full-screen').addEventListener('mousemove', colorChange);
+window.addEventListener('keydown', event =>{
+  if(event.key === 'Escape'){
+      colorToggle;
+  }
+});
